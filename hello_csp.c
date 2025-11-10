b5db981563c0d2c5e048c496dc8031cc2caf2ce5
quimsonchin <k_quimso@live.concordia.ca>
2025-11-10 20:04:20 +0000

    recciver and sender

--- hello_csp.c
+++
@@ -1,9 +0,0 @@
-
-#include <stdio.h>
-#include <csp/csp.h>
-
-int main(void) {
-    csp_init();  // returns void
-    printf("Hello, CSP is built and initialized!\n");
-    return 0;
-}
---
+++ receiver_csp.c
@@ -0,0 +1,9 @@
+
+#include <stdio.h>
+#include <csp/csp.h>
+
+int main(void) {
+    csp_init();  // returns void
+    printf("Hello, ihoihiohoi CSP is built and initialized!\n");
+    return 0;
+}
